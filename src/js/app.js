ae66fbcdd8995903d2fea1b79b48eaa5fc895e70
Aaron Glenn <aaron@tout.com>
2011-09-15 23:08:32 -0700

    Sliding around

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,18 @@
 (function ($) {
   
   App = Backbone.View.extend({
+
+    setup_navigation: function(){
+      $('#navigation a').click(function(e){
+          var page = e.target.href.split('#')[1];
+          $('html, body').animate({scrollTop: $('#'+page).offset().top}, 500);  
+          e.preventDefault();
+        }
+      );
+    },
     
     initialize: function () {
-      // ...
+      this.setup_navigation();
     }
   });
 
