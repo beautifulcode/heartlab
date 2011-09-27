18074c7b14f9131f01821f83e70bd9b37d544a9b
Aaron Glenn <aaron@tout.com>
2011-09-26 22:24:42 -0700

    Parallax stuff

---
+++ src/js/support/parallax.js
@@ -0,0 +1,26 @@
+﻿// The plugin code
+(function($){
+    $.fn.parallax = function(options){
+        var $$ = $(this);
+        offset = $$.offset();
+        var defaults = {
+            "start": 0,
+            "stop": offset.top + $$.height(),
+            "coeff": 0.95
+        };
+        var opts = $.extend(defaults, options);
+        return this.each(function(){
+            $(window).bind('scroll', function() {
+                windowTop = $(window).scrollTop();
+                if((windowTop >= opts.start) && (windowTop <= opts.stop)) {
+                    newCoord = windowTop * opts.coeff;
+                    $$.css({
+                        "background-position": "0 "+ newCoord + "px"
+                    });
+                }
+            });
+        });
+    };
+})(jQuery);
+
+
